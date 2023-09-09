fa9c348f26f5507fe908e6bed2a968b9b1fd6f64
Walter <walterlaicollege@gmail.com>
2023-09-09 17:39:14 -0400

    added function to get dataframe of title author abstract from a single page

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 from bs4 import BeautifulSoup
+import pandas as pd
 
 filename="1.html"
-html_text = open(filename,"r",encoding='utf-8').read()
 
-soup = BeautifulSoup(html_text, 'html.parser')
-paragraph_list = soup.find_all(attrs={"itemprop": "description"})
+def get_title_author_abstract(filename):
+    '''
+    Input is the filename like 2019-02-04 <Year>-<Season>-<Section>
+    Output is a Pandas DataFrame that looks like this:
+    title     | authors | abstract
+    ---------------------------------------------
+    Batteries | Walter  | Results from paper
+    Potentials| Zicheng | More results from paper
+    '''
+    html_text = open(filename,"r",encoding='utf-8').read()
+    soup = BeautifulSoup(html_text, 'html.parser')
+    abstract_list = soup.find_all(attrs={"itemprop": "description"})
+    title_list = soup.find_all(attrs={"class":"art-list-item-title"})
+    authors_list = soup.find_all(attrs={"class":"art-list-item-meta"})
+    table = []
+    for title, author, abstract in zip(title_list, authors_list, abstract_list):
+        row = []
+        row.append(title.text)
+        row.append(author.text)
+        row.append(abstract.text)
+        table.append(row)
+    dataframe = pd.DataFrame(data=table,columns=['title','authors','abstract'])
+    return dataframe
